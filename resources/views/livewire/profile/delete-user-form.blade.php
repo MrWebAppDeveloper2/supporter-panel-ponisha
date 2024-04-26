a8e51c30c3aa1c09a56aaa51434e794623df8048
anounymous <anounymous@none.com>
2024-04-26 16:04:19 +0330

    feat(branch:feature/auth) -> translate delete account seciton in profile page to Persian.

--- resources/views/livewire/profile/delete-user-form.blade.php
+++ resources/views/livewire/profile/delete-user-form.blade.php
@@ -26,28 +26,28 @@ new class extends Component
 <section class="space-y-6">
     <header>
         <h2 class="text-lg font-medium text-gray-900">
-            {{ __('Delete Account') }}
+            حذف حساب
         </h2>
 
         <p class="mt-1 text-sm text-gray-600">
-            {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain.') }}
+            پس از حذف حساب تمامی اطلاعات و فعالیت های شما نیز حذف می شود و قابل بازگشت نیست.
         </p>
     </header>
 
     <x-danger-button
         x-data=""
         x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')"
-    >{{ __('Delete Account') }}</x-danger-button>
+    >حذف حساب</x-danger-button>
 
     <x-modal name="confirm-user-deletion" :show="$errors->isNotEmpty()" focusable>
         <form wire:submit="deleteUser" class="p-6">
 
             <h2 class="text-lg font-medium text-gray-900">
-                {{ __('Are you sure you want to delete your account?') }}
+                آیا از حذف حساب کاربری خود مطمئن هستید ؟
             </h2>
 
             <p class="mt-1 text-sm text-gray-600">
-                {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your account.') }}
+                پس از حذف حساب تمامی اطلاعات و فعالیت های شما نیز حذف می شود و قابل بازگشت نیست.
             </p>
 
             <div class="mt-6">
@@ -67,11 +67,11 @@ new class extends Component
 
             <div class="mt-6 flex justify-end">
                 <x-secondary-button x-on:click="$dispatch('close')">
-                    {{ __('Cancel') }}
+                    انصراف
                 </x-secondary-button>
 
                 <x-danger-button class="ms-3">
-                    {{ __('Delete Account') }}
+                    حذف
                 </x-danger-button>
             </div>
         </form>
