fe9924fa121561663a992b68e375ef3c8614e203
anounymous <anounymous@none.com>
2024-04-26 16:15:13 +0330

    feat(branch:feature/auth) -> translate reset password pages to Persian.

--- resources/views/livewire/pages/auth/forgot-password.blade.php
+++ resources/views/livewire/pages/auth/forgot-password.blade.php
@@ -36,26 +36,31 @@ new #[Layout('layouts.guest')] class extends Component
     }
 }; ?>
 
-<div>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-    </div>
-
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form wire:submit="sendPasswordResetLink">
-        <!-- Email Address -->
+<div class="card">
+    <div class="card-body">
         <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input wire:model="email" id="email" class="block mt-1 w-full" type="email" name="email" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            <div class="mb-4 text-sm text-gray-600">
+                مشکلی نیست ! ایمیل حساب خود را وارد کنید و لینک بازنشانی کلمه عبور را دریافت کنید.
+            </div>
+
+            <!-- Session Status -->
+            <x-auth-session-status class="mb-4" :status="session('status')" />
+
+            <form wire:submit="sendPasswordResetLink">
+                <!-- Email Address -->
+                <div>
+                    <label for="email">ایمیل</label>
+                    <x-text-input wire:model="email" id="email" class="block mt-1 w-full" type="email" name="email" required autofocus />
+                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                </div>
+
+                <div class="flex items-center justify-end mt-4">
+                    <x-primary-button>
+                        ارسال لینک بازنشانی
+                    </x-primary-button>
+                </div>
+            </form>
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
-    </form>
+    </div>
 </div>
--- resources/views/livewire/profile/update-profile-information-form.blade.php
+++ resources/views/livewire/profile/update-profile-information-form.blade.php
@@ -199,17 +199,17 @@ new class extends Component {
             @if (auth()->user() instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! auth()->user()->hasVerifiedEmail())
                 <div>
                     <p class="text-sm mt-2 text-gray-800">
-                        {{ __('Your email address is unverified.') }}
+                        ایمیل شما احراز نشده است.
 
                         <button wire:click.prevent="sendVerification"
                                 class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                            {{ __('Click here to re-send the verification email.') }}
+                            برای ارسال لینک فعال سازی کلیک کنید.
                         </button>
                     </p>
 
                     @if (session('status') === 'verification-link-sent')
                         <p class="mt-2 font-medium text-sm text-green-600">
-                            {{ __('A new verification link has been sent to your email address.') }}
+                            لینک فعال سازی به آدرس ایمیل ثبت شده ارسال شد.
                         </p>
                     @endif
                 </div>
