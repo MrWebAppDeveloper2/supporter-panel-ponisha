f3a8895dce5e8680a0ce4703696dc535ce6b5495
anounymous <anounymous@none.com>
2024-04-26 17:08:51 +0330

    feat(branch:feature/permission) -> creat admin role with all permissions and asign to he in DatabaseSeeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\Customer;
+use App\Models\Permission;
+use App\Models\Role;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,20 +17,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // initial permissions
+        $this->call(PermissionSeeder::class);
 
-        User::factory()->create([
+        // create admin role with all permissions
+        $adminRole = Role::create(['name' => 'admin-role']);
+        $adminRole->permissions()->sync(Permission::all());
+
+        // create admin user
+        $admin = User::factory()->create([
             'name' => 'Test Admin',
             'email' => 'admin@example.com',
             'password'  => Hash::make('123456789')
         ]);
 
+        // associate admin role to admin user
+        $admin->role()->associate($adminRole);
+        $admin->save();
+
+        // create customer user for example
         User::factory()->customer()->create([
             'name' => 'Test Customer',
             'email' => 'customer@example.com',
             'password'  => Hash::make('123456789')
         ]);
-
-        $this->call(PermissionSeeder::class);
     }
 }
