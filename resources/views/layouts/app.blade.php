ddcd744cba379b0c4e0ac8bc2aa95e910ea1649a
anounymous <anounymous@none.com>
2024-04-26 14:35:02 +0330

    feat:(branch:feature/install-dashboard-ui) -> dashboard ui installed.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,116 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="light-style customizer-hide" dir="rtl" data-theme="theme-default" data-assets-path="../../assets/" data-template="horizontal-menu-template">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <meta name="description" content="">
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="{{ asset('assets/img/favicon/favicon.ico')}}">
+
+    <!-- Icons -->
+    <link rel="stylesheet" href="{{ asset('assets/vendor/fonts/boxicons.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/fonts/fontawesome.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/fonts/flag-icons.css')}}">
+
+    <!-- Core CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/vendor/css/rtl/core.css')}}" class="template-customizer-core-css">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/css/rtl/theme-default.css')}}"
+          class="template-customizer-theme-css">
+    <link rel="stylesheet" href="{{ asset('assets/css/demo.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/css/rtl/rtl.css')}}">
+
+    <!-- Vendors CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/typeahead-js/typeahead.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/apex-charts/apex-charts.css')}}">
+
+    <!-- Helpers -->
+    <script src="../../assets/vendor/js/helpers.js"></script>
+
+    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+    <!--? Template customizer: To hide customizer set displayCustomizer value false in config.js.  -->
+    <script src="../../assets/vendor/js/template-customizer.js"></script>
+    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+    <script src="../../assets/js/config.js"></script>
+
+    <!-- Scripts -->
+    @vite(['resources/js/app.js'])
+
+    @yield('style')
+</head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            <livewire:layout.navigation />
+        <!-- Layout wrapper -->
+        <div class="layout-wrapper layout-navbar-full layout-horizontal layout-without-menu">
+            <div class="layout-container">
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
+                <!-- Navbar -->
+                <livewire:layout.navigation />
+                <!-- / Navbar -->
+
+                <!-- Layout container -->
+                <div class="layout-page">
+                    <!-- Content wrapper -->
+                    <div class="content-wrapper">
+                        <!-- Menu -->
+                        <livewire:layout.header/>
+                        <!-- / Menu -->
+
+                        <!-- Content -->
+
+                        <div class="container-xxl flex-grow-1 container-p-y">
+                            <div class="row px-3">
+                                {{ $slot }}
+                            </div>
+                        </div>
+                        <!--/ Content -->
+
+                        <div class="content-backdrop fade"></div>
                     </div>
-                </header>
-            @endif
+                    <!--/ Content wrapper -->
+                </div>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+                <!--/ Layout container -->
+            </div>
         </div>
+
+        <!-- Overlay -->
+        <div class="layout-overlay layout-menu-toggle"></div>
+
+        <!-- Drag Target Area To SlideIn Menu On Small Screens -->
+        <div class="drag-target"></div>
+
+        <!--/ Layout wrapper -->
+
+        <!-- Core JS -->
+        <!-- build:js assets/vendor/js/core.js -->
+        <script src="{{ asset('assets/vendor/libs/jquery/jquery.js')}}"></script>
+        <script src="{{ asset('assets/vendor/libs/popper/popper.js')}}"></script>
+        <script src="{{ asset('assets/vendor/js/bootstrap.js')}}"></script>
+        <script src="{{ asset('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js')}}"></script>
+
+        <script src="{{ asset('assets/vendor/libs/hammer/hammer.js')}}"></script>
+
+        <script src="{{ asset('assets/vendor/libs/i18n/i18n.js')}}"></script>
+        <script src="{{ asset('assets/vendor/libs/typeahead-js/typeahead.js')}}"></script>
+
+        <script src="{{ asset('assets/vendor/js/menu.js')}}"></script>
+        <!-- endbuild -->
+
+        <!-- Vendors JS -->
+        <script src="{{ asset('assets/vendor/libs/apex-charts/apexcharts.js')}}"></script>
+        <script src="{{ asset('assets/vendor/libs/formvalidation/dist/js/plugins/Bootstrap5.min.js') }}"></script>
+        <script src="{{ asset('assets/vendor/libs/formvalidation/dist/js/plugins/AutoFocus.min.js') }}"></script>
+
+        <!-- Main JS -->
+        <script src="{{ asset('assets/js/main.js')}}"></script>
+
+        <!-- Page JS -->
+        <script src="{{ asset('assets/js/pages-auth.js') }}"></script>
+
+        @yield('script')
     </body>
 </html>
