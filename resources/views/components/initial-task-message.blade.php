7ee64a439c199a77de9be6e63ef0210736c375b3
anounymous <anounymous@none.com>
2024-05-06 13:07:35 +0330

    fix(branch:feature/task) -> creator name in initial ticket and task message color bug fixed.

--- resources/views/components/initial-task-message.blade.php
+++ resources/views/components/initial-task-message.blade.php
@@ -3,7 +3,7 @@
         <ul class="list-unstyled">
             <li>وظیفه شماره: {{ $task->id }}</li>
             <li>عنوان وظیفه: {{ $task->title }}</li>
-            <li>ایجاد کننده: <a href="#" class="text-white">{{ $task->creator->name }}</a></li>
+            <li>ایجاد کننده: <a href="#" class="text-secondary">{{ $task->creator->name }}</a></li>
             <li>تاریخ ایجاد: {{ $task->created_at }}</li>
         </ul>
         <br>
