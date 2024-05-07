f8973ceac33fcd48d7a351ba530d40555bbe720d
anounymous <anounymous@none.com>
2024-05-07 16:37:55 +0330

    refactor(branch:dev) -> remove notificaiton section in navigation and add redirect route / to /dashboard

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
+Route::redirect('/', '/dashboard');
 
 Route::middleware(['auth', 'verified'])->group(function(){
     Route::view('dashboard', 'dashboard')
